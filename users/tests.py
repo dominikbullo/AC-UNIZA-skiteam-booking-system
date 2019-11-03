28f6e76a23d60f9e1f25f292bf404fe77491bbf3
Dominik <dominobullo@gmail.com>
2019-11-03 12:08:38 +0100

    added one test for registration
    Took 6 minutes

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+
+class RegistrationTestCase(APITestCase):
+
+    def test_registration(self):
+        data = {"username": "testcase", "email": "test@localhost.app",
+                "password1": "some_strong_psw", "password2": "some_strong_psw"}
+        response = self.client.post("/api/rest-auth/registration/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
