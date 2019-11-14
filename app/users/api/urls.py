c9d81e54461405d838dc1928df5dea5220fef1c1
Dominik <dominobullo@gmail.com>
2019-11-14 11:50:53 +0100

    correcting the tests
    Took 3 minutes

--- app/users/api/urls.py
+++ app/users/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from users.api.views import CurrentUserAPIView
+from users.api.views import CurrentUserAPIView, ProfileList
 
 urlpatterns = [
     path("user/", CurrentUserAPIView.as_view(), name="current-user"),
-    # path("profiles/", ProfileList.as_view(), name="profile-list")
+    path("profiles/", ProfileList.as_view(), name="profile-list")
 ]
--- app/users/api/views.py
+++ app/users/api/views.py
@@ -3,12 +3,17 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-# from users.api.serializers import ProfileSerializer
+from users.api.serializers import ProfileSerializer
 from users.api.serializers import UserDisplaySerializer
+from users.models import Profile
 
 
-#
-#
+class ProfileList(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+
 class CurrentUserAPIView(APIView):
 
     def get(self, request):
