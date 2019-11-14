b5d5fff4379b8ff337471cfa84520813dda6e8dc
Dominik <dominobullo@gmail.com>
2019-11-14 22:22:33 +0100

    add /register path to also register
    Took 23 minutes

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,23 +1,20 @@
-from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 from django.urls import path, include
 
-from users import views as user_views
+from allauth.account.views import SignupView
 
 # TODO: email registration
 # https://django-registration.readthedocs.io/en/3.0/activation-workflow.html
 from django_registration.backends.one_step.views import RegistrationView
 
-from users.forms import CustomUserCreationForm
-
 # https://wsvincent.com/django-login-with-email-not-username/
-
 
 urlpatterns = [
     # Login via browser
     path("", include('allauth.urls')),
-    # path("", include("django.contrib.auth.urls")),
 
-    path("api/", include("users.api.urls")),
+    # Duplicating reg url -> there are /register and /signup
+    path('register/', SignupView.as_view(), name="register"),
 
     # Login via browsable api
     path("api-auth/", include("rest_framework.urls")),
