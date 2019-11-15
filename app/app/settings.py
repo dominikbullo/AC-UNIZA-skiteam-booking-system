63c0e2115f75645cfb4804976132e68c38a4a912
Dominik <dominobullo@gmail.com>
2019-11-15 18:46:10 +0100

    add family to setting and fix urls
    Took 6 minutes

--- app/app/settings.py
+++ app/app/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+
     'users',
+    'family',
 
     'rest_framework',
     'rest_framework.authtoken',
