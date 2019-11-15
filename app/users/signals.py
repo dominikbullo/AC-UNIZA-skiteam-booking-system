ae1b32f362f8e6ea927c271a8d666683a7ea045f
Dominik <dominobullo@gmail.com>
2019-11-15 08:22:06 +0100

    create token for every user
    Took 24 minutes

--- app/users/signals.py
+++ app/users/signals.py
@@ -1,12 +1,19 @@
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from users.models import User, Profile
+from rest_framework.authtoken.models import Token
+
+from users.models import Profile
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_profile(sender, instance, created, **kwargs):
     print("Created: ", created)
     if created:
+        # Create profile for every user
         Profile.objects.create(user=instance)
-        # Parent.objects.create(user=instance)
+
+        # RES: https://www.django-rest-framework.org/api-guide/authentication/
+        # Generate token for every user
+        Token.objects.get_or_create(user=instance)
