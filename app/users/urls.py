7061da671e1f39e98207a1776f9599070dffb15f
Dominik <dominobullo@gmail.com>
2019-11-15 08:22:37 +0100

    rearranged URLs for user, delete /register url
    Took 32 seconds

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,5 +1,6 @@
-from django.shortcuts import redirect
+from django.conf.urls import url
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 from allauth.account.views import SignupView
 
@@ -10,15 +11,14 @@ urlpatterns = [
     # Login via browser
     path("", include('allauth.urls')),
 
-    # Duplicating reg url -> there are /register and /signup
-    path('register/', SignupView.as_view(), name="register"),
-
     # Login via browsable api
     path("api-auth/", include("rest_framework.urls")),
 
     # Login via REST
     path("api/rest-auth/", include("rest_auth.urls")),
 
-    # # Registration via REST
-    # path("api/rest-auth/signup/", include("rest_auth.registration.urls")),
+    # Registration via REST
+    path("api/rest-auth/signup/", include("rest_auth.registration.urls")),
+
+    # url(r'^api-token-auth/', obtain_auth_token)
 ]
