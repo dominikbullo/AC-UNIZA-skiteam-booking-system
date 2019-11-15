25defd9efa4ab28edbf2a6a7d9be1fd531183905
Dominik <dominobullo@gmail.com>
2019-11-15 17:44:08 +0100

    disable creating user via API
    Took 33 minutes

--- app/users/api/urls.py
+++ app/users/api/urls.py
@@ -3,7 +3,9 @@ from users.api.views import *
 
 urlpatterns = [
     path("", CurrentUserAPIView.as_view(), name="current-user"),
-    path('create/', CreateUserView.as_view(), name='create'),
+
+    # I don't need to create token, because i already have it when creating user
+    # path('create/', CreateUserView.as_view(), name='create'),
     path('token/', CreateTokenView.as_view(), name='token'),
     path('me/', ManageUserView.as_view(), name='me'),
 ]
