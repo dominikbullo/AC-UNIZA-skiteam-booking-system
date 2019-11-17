e33c46d36771fd8d15aa206ea7ae3afe4c02e043
Dominik <dominobullo@gmail.com>
2019-11-17 18:21:11 +0100

    move family stuff to family app
    Took 1 minute

--- app/family/models.py
+++ app/family/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Family(models.Model):
+    id = models.AutoField(primary_key=True)
+
+
+class FamilyMember(models.Model):
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    relationship = models.CharField(max_length=100)
+
+
+class Parent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    children = models.ManyToManyField('self', null=True, blank=True, related_name='c', symmetrical=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Child:
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    parents = models.ManyToManyField('self', null=True, blank=True, related_name='p', symmetrical=False)
+    pass
+
+
+class Coach:
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
--- app/users/models.py
+++ app/users/models.py
@@ -23,31 +23,3 @@ class Profile(models.Model):
 
     def __str__(self):  # __unicode__ for Python 2
         return self.user.email
-
-
-class Family(models.Model):
-    id = models.AutoField(primary_key=True)
-
-
-class FamilyMember(models.Model):
-    profile = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    relationship = models.CharField(max_length=100)
-
-
-class Parent(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    children = models.ManyToManyField('self', null=True, blank=True, related_name='c', symmetrical=False)
-
-    def __str__(self):
-        return self.user.username
-
-
-class Child:
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    parents = models.ManyToManyField('self', null=True, blank=True, related_name='p', symmetrical=False)
-    pass
-
-
-class Coach:
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
