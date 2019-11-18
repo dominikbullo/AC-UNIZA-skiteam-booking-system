a7c506b66799f0decbaf5e2bf63b4bc9f34b9ad3
Dominik <dominobullo@gmail.com>
2019-11-18 21:21:25 +0100

    editing to models
    Took 1 minute

--- app/family/models.py
+++ app/family/models.py
@@ -25,6 +25,9 @@ class Parent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=30, blank=True)
 
+    def __str__(self):
+        return self.user.username
+
 
 class Child(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
