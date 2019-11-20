300be8f60870984f0244979f10dac45f986ef1e1
Dominik <dominobullo@gmail.com>
2019-11-20 19:33:51 +0100

    cleanup
    Took 1 minute

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,11 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-
-from rest_framework.routers import DefaultRouter
-
-from users.api.views import ProfileViewSet
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
