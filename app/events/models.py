33d49b47529af1186a19bbba26a2029b21c6a50e
Dominik <dominobullo@gmail.com>
2019-12-04 09:43:26 +0100

    added event structure jsut as comments
    Took 11 minutes

--- app/events/models.py
+++ app/events/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Season(models.Model):
+    # year
+    # start date
+    # end date
+    pass
+
+
+class Event(models.Model):
+    # TODO should i create new table for every training or could i just add "type"
+    # name
+    # start
+    # end
+    # place
+    # type
+    # skis_type
+    # canceled
+
+    class Meta:
+        abstract = True
+
+
+class Category(models.Model):
+    # name
+    # year from season
+    # year from to know who add into this cat
+    # year until to know who add into this cat
+    pass
