16bb073b0800af41d4992ffcd70750614fe027ea
Dominik <dominobullo@gmail.com>
2020-02-27 16:59:14 +0100

    test page for api calls
    Took 3 hours 51 minutes

--- app/frontend/src/views/pages/Page2.vue
+++ app/frontend/src/views/pages/Page2.vue
@@ -1,3 +1,36 @@
 <template>
-    <h4>You are in Page2.</h4>
+    <div id="dashboard">
+        <h1>That's the dashboard!</h1>
+        <p>You should only get here if you're authenticated!</p>
+        <p>Your email address: {{ email }}</p>
+    </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        email: ''
+      }
+    },
+    created() {
+      this.$http.get('/user/full-info/', {headers: {"Authorization": `Bearer ${JWTToken}`}})
+        .then(res => {
+          // console.log(res);
+          const data = res.data.results;
+          // console.log(data);
+        })
+        .catch(error => console.log(error))
+    }
+  }
+</script>
+
+<style scoped>
+    h1, p {
+        text-align: center;
+    }
+
+    p {
+        color: red;
+    }
+</style>
