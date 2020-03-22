3f6c42d6116cb3d6767d9c82e8ba748b96b53461
Dominik <dominobullo@gmail.com>
2020-03-22 22:06:26 +0100

    DISABLED: Events vies, models, urls
    Took 2 hours 12 minutes

--- backend/events/api/urls.py
+++ backend/events/api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from events.api.views import EventsViewSet
+# from events.api.views import EventsViewSet
 
 app_name = 'events'
 
 # https://www.django-rest-framework.org/api-guide/routers/
 router = DefaultRouter()
-router.register(r"events", EventsViewSet, basename="event")
+# router.register(r"events", EventsViewSet, basename="event")
 
 urlpatterns = [
     path("", include(router.urls)),
--- backend/events/api/views.py
+++ backend/events/api/views.py
@@ -3,16 +3,16 @@ from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from events.models import SkiTraining
-from events.api.serializers import EventSerializer
-from events.api.permissions import IsOwnerOrReadOnly, IsOwnFamilyOrReadOnly
-
-
-# https://github.com/LondonAppDeveloper/recipe-app-api/blob/master/app/recipe/views.py
-class EventsViewSet(viewsets.ModelViewSet):
-    queryset = SkiTraining.objects.all()
-    serializer_class = EventSerializer
-    # permission_classes = [IsOwnFamilyOrReadOnly]
-    # filter_backends = [SearchFilter]
-    # search_fields = ["name"]
-    pass
+# from events.models import SkiTraining
+# from events.api.serializers import EventSerializer
+# from events.api.permissions import IsOwnerOrReadOnly, IsOwnFamilyOrReadOnly
+#
+#
+# # https://github.com/LondonAppDeveloper/recipe-app-api/blob/master/app/recipe/views.py
+# class EventsViewSet(viewsets.ModelViewSet):
+#     queryset = SkiTraining.objects.all()
+#     serializer_class = EventSerializer
+#     # permission_classes = [IsOwnFamilyOrReadOnly]
+#     # filter_backends = [SearchFilter]
+#     # search_fields = ["name"]
+#     pass
