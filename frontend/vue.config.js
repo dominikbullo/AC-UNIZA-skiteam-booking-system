13e701e9d883b7ff6166fc944fc4da699052b66d
Dominik <dominobullo@gmail.com>
2020-05-17 21:19:02 +0200

    added docker-compose-prod-https.yml which should work with docker proxy to serve via HTTPS, delete pwa config from vue.config.js because it should work without it
    Took 7 minutes

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -32,20 +32,5 @@ module.exports = {
       poll: 1000,
       ignored: '/app/node_modules/'
     }
-  },
-  pwa: {
-    name: 'My App',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js'
-      // ...other Workbox options...
-    }
   }
 }
